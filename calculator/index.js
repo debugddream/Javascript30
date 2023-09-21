a3645ce6ae40c3cd0cf8049afecc6acb80e7c07c
Hammadulhaq123 <professorcoding123@gmail.com>
2023-09-21 17:07:43 +0500

    Updated the if block on line 21.
    I mistakenly forgot to write line 22 & 23 in the tutorial this is the fixed code and everything is working fine.

--- calculator/index.js
+++ calculator/index.js
@@ -2,30 +2,32 @@ const history = document.getElementById("prev");
 const result = document.getElementById("result");
 const buttons = document.querySelectorAll(".btns");
 
+
 let string = "";
+
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", function () {
         if (btn.innerHTML == "=") {
-            history.innerHTML = string
-            string = eval(string)
-            result.innerHTML = string
+            history.innerHTML = string;
+            string = eval(string);
+            result.innerHTML = string;
         }
         else if (btn.innerHTML == "AC") {
-            string = ""
-            result.innerHTML = "0"
-
+            string = "";
+            result.innerHTML = "0";
         }
         else {
-            if (string.length > 9) {
-                result.style.fontSize = "40px"
-
+            if (string.length > 8) {
+                result.style.fontSize = "45px";
+                string += btn.innerHTML;
+                result.innerHTML = string;
             }
-            string += btn.innerHTML
-            result.innerHTML = string
+            else {
+                string += btn.innerHTML;
+                result.innerHTML = string;
+            }
         }
     })
 })
-
-
 
